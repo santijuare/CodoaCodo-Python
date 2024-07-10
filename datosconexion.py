57ca53e03a0ad649ab0a2e6f8e544f1eabe3ab7b
andrespienizzio <andrespienizzio@gmail.com>
2024-07-10 18:23:49 -0300

    Try to fix the error in add products - third try

--- datosconexion.py
+++ datosconexion.py
@@ -5,6 +5,3 @@ database='Tiagos84$miapp'
 usarname= 'Tiagos84'
 api_token = '99bec0f7d64a06eb3852481a3dae4efdd2df81b4'
 domain_name = 'Tiagos84.pythonanywhere.com'
-
-# colocar aqui los datos de conexion a la base de datos
-# luego renombrar el archivo a datosconexion.py
